c6748cbbe4cf1dffae2e2be6d68da96673d3b1b2
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-06-01 19:51:21 +0200

    feat: :sparkles: Add new features to Fruit Entity to convert this Entity in a PanacheEntity too

--- src/main/java/org/acme/rest/json/entities/Fruit.java
+++ src/main/java/org/acme/rest/json/entities/Fruit.java
@@ -1,8 +1,33 @@
 package org.acme.rest.json.entities;
 
-public class Fruit extends{
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="Fruit")
+@JsonPropertyOrder({"name", "description"})
+public class Fruit extends PanacheEntity{
+
+    /*
+    * Las propiedades/atributos deben ser public para que Jackson pueda acceder a ellas mediante Reflection y podeer realizar la serialización o deserialización donde Quarkus los hace PUBLIC
+    * */
+
+    @NotEmpty
+    @NotBlank
+    @Column(unique = true, name="name")
     public String name;
+
+    @NotEmpty
+    @NotBlank
+    @Column(name="description")
     public String description;
 
     public Fruit() {
@@ -11,5 +36,13 @@ public class Fruit extends{
     public Fruit(String name, String description) {
         this.name = name;
         this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
