875224c3b7576baa3ff204c9a5129a81b522a4bb
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-06-06 20:48:19 +0200

    refactor: :recycle: Refactor ServiceStudent to update it with DAO pattern

--- src/main/java/org/acme/rest/json/repositories/RepositoryStudent.java
+++ src/main/java/org/acme/rest/json/repositories/RepositoryStudent.java
@@ -3,12 +3,19 @@ package org.acme.rest.json.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.acme.rest.json.entities.Student;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Sort;
 
+@ApplicationScoped
 public class RepositoryStudent implements PanacheRepository<Student> {
+
+    // There is not need to create a method to call panache methods because Repository Student implements PanacheRepository already have this methods lika persist() which can be used and called directly in the layer Service
+
+    // It's also not neccesary called the constructor to let this class become a CDI because PanacheRepository will be managed.
 
     public List<Student> listAllOrderedByName() {
         return this.listAll(Sort.by("name").ascending());
@@ -24,4 +31,5 @@ public class RepositoryStudent implements PanacheRepository<Student> {
             this.delete(student.get());
         }
     }
+
 }
