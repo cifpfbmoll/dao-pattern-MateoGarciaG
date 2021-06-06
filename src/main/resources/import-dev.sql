b2eb20c13986ab61dffedb9bc9f32ac69070aabf
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-06-06 11:33:25 +0200

    refactor: :recycle: Change Fruits data definition in Scripts by Students Schema DB, also in application.properties

--- src/main/resources/import-dev.sql
+++ src/main/resources/import-dev.sql
@@ -1,13 +1,15 @@
-DROP TABLE IF EXISTS Fruit;
+DROP TABLE IF EXISTS Student;
 
-CREATE TABLE Fruit
+CREATE TABLE Student
 (
     id SERIAL NOT NULL,
     name VARCHAR (255) NOT NULL,
-    description VARCHAR(255),
+    surname VARCHAR(255),
+    date_birth DATE,
+    phone VARCHAR(100),
     PRIMARY KEY (id)
 );
-INSERT INTO Fruit (id, name, description)
-VALUES (1050, 'Apple', 'Winter fruit');
-INSERT INTO Fruit (id, name, description)
-VALUES (2050, 'Pineapple', 'Tropical fruit');
+INSERT INTO Student (id, name, surname, date_birth, phone)
+VALUES (1050, 'Mateo', 'Alvarez', '2005-06-05', '+34 666666666');
+INSERT INTO Student (id, name, surname, date_birth, phone)
+VALUES (2050, 'Will', 'Smith', '1999-06-17', '+34 677878997');
